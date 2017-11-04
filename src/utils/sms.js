567a31e09bf8a07e6ed9f4d9ebdc55ead5563364
Rafiya <rafiyajaved@gmail.com>
2017-11-04 08:20:29 -0400

    entered accountSID/authToken for Twilio to sms.js

--- src/utils/sms.js
+++ src/utils/sms.js
@@ -1,40 +1,42 @@
-var accountSid = "";
-var authToken = "";
+var accountSid = "ACebb6ec186e9eb445f028f383dc5d6dc0";
+var authToken = "6bbf2e8266899ba9ff08ff5753359e79";
 // var client = require('twilio');
 
-let url = "https://api.twilio.com/2010-04-01/Accounts/ACebb6ec186e9eb445f028f383dc5d6dc0/Messages";
+let url =
+  "https://api.twilio.com/2010-04-01/Accounts/ACebb6ec186e9eb445f028f383dc5d6dc0/Messages";
 
 export function sendSMS(data, phoneNumberString) {
 
-    //TODO: Validate phone number string is in correct format
+  //TODO: Validate phone number string is in correct format
 
-    console.log(phoneNumberString);
+  console.log(phoneNumberString);
 
-    $.ajax({
-        type: 'POST',
-        url: url,
-        data: {
-            "To" : "+1" + phoneNumberString,
-            "From" : "14044589195",
-            "Body" : formatData(data)
-        },
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader ("Authorization", "Basic " + btoa(accountSid + ':' + authToken));
-        },
-        success: function(data) {
-            console.log(data);
-        },
-        error: function(data) {
-            console.log(data);
-        }
-    });
+  $.ajax({
+    type: 'POST',
+    url: url,
+    data: {
+      "To": "+1" + phoneNumberString,
+      "From": "14044589195",
+      "Body": formatData(data)
+    },
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("Authorization", "Basic " + btoa(accountSid +
+        ':' + authToken));
+    },
+    success: function(data) {
+      console.log(data);
+    },
+    error: function(data) {
+      console.log(data);
+    }
+  });
 }
 
 function formatData(data) {
-    var formattedString = "";
-    formattedString = formattedString + data.name + "\n";
-    formattedString = formattedString + data.phone + "\n";
-    formattedString = formattedString + data.civic_address;
-    console.log(formattedString);
-    return formattedString;
+  var formattedString = "";
+  formattedString = formattedString + data.name + "\n";
+  formattedString = formattedString + data.phone + "\n";
+  formattedString = formattedString + data.civic_address;
+  console.log(formattedString);
+  return formattedString;
 }
